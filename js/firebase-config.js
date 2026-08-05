29fe6eb04398494a65a1c297e0dd30834df42d96
Gcina Banele Kubeka <137442411+Banele1815@users.noreply.github.com>
2026-08-05 12:10:55 +0200

    added shop.html

---
+++ js/firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDMGaL0KYmJakJt4wqefY7-mL-XKDCqlTc",
+  authDomain: "urbanthreadsstore-5d0c2.firebaseapp.com",
+  projectId: "urbanthreadsstore-5d0c2",
+  storageBucket: "urbanthreadsstore-5d0c2.firebasestorage.app",
+  messagingSenderId: "1093800275703",
+  appId: "1:1093800275703:web:23e9e5ddff2756c09dedf2"
+};
