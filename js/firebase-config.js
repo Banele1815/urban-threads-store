04c4cf15a9d69f43012ece1b55a7753519fe787d
Gcina Banele Kubeka <137442411+Banele1815@users.noreply.github.com>
2026-08-05 14:30:57 +0200

    updated login.html

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,3 +1,9 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
+
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-auth.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDMGaL0KYmJakJt4wqefY7-mL-XKDCqlTc",
   authDomain: "urbanthreadsstore-5d0c2.firebaseapp.com",
@@ -6,3 +12,9 @@ const firebaseConfig = {
   messagingSenderId: "1093800275703",
   appId: "1:1093800275703:web:23e9e5ddff2756c09dedf2"
 };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
